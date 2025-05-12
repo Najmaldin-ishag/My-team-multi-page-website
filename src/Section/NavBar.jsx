8ecd818c15915b2fb46a25874366994fd41a6556
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-12 03:05:08 +0300

    Update NavBar.jsx

--- src/Section/NavBar.jsx
+++ src/Section/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="w-full bg-primary-midnight-green p-10">
       <nav className=" container flex items-center justify-between">
-        <div className="flex items-center justify-between gap-5">
+        <div className="flex items-center justify-between gap-[3.5rem]">
           <img src="./assets/logo.svg" alt="logo" />
           <span className="text-m text-primary-white font-semibold">Home</span>
           <span className="text-m text-primary-white  font-semibold">
