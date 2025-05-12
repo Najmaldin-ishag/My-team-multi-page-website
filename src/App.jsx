05b21d6048a1c9e7c1e55a615b01b1a40ca4a131
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-13 02:03:34 +0300

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Cta from "./Section/Cta";
 import Features from "./Section/Features";
 import Footer from "./Section/Footer";
+
 import Hero from "./Section/Hero";
 import NavBar from "./Section/NavBar";
 import Testimonials from "./Section/Testimonials";
