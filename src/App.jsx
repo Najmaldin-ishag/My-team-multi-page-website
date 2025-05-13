7581fcbfc1be6e2744a3da59eccf90c5760fa61b
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-14 02:25:17 +0300

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import Cta from "./Section/Cta";
-import Features from "./Section/Features";
-import Footer from "./Section/Footer";
+import { Routes, Route } from "react-router-dom";
+// import Cta from "./Section/Cta";
+// import Features from "./Section/Features";
+// import Footer from "./Section/Footer";
 
-import Hero from "./Section/Hero";
-import NavBar from "./Section/NavBar";
-import Testimonials from "./Section/Testimonials";
-
+// import Hero from "./Section/Hero";
+// import NavBar from "./Section/NavBar";
+// import Testimonials from "./Section/Testimonials";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import ContactUs from "./Pages/ContactUs";
+import MainLayout from "./Layouts/MainLayout";
 const App = () => {
   return (
     <>
-      <NavBar />
-      <Hero />
-      <main>
-        <Features />
-        <Testimonials />
-        <Cta />
-        <Footer />
-      </main>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Route>
+      </Routes>
     </>
   );
 };
