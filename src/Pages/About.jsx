eaec494a1b2a290d952d73dc282d48a5f06baad3
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-14 02:26:05 +0300

    Update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,13 @@
+import Hero from "../AboutSection/Hero";
+import Team from "../AboutSection/Team";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
 };
 
 export default About;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,17 @@
+import Cta from "../HeroSection/Cta";
+import Features from "../HeroSection/Features";
+import Hero from "../HeroSection/Hero";
+import Testimonials from "../HeroSection/Testimonials";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Testimonials />
+      <Cta />
+    </>
+  );
 };
 
 export default Home;
