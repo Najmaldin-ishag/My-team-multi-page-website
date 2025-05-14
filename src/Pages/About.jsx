5f71aa5dd6430a2256879e8d6e7f5b5525ee8983
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-14 22:51:51 +0300

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,15 @@
 import Hero from "../AboutSection/Hero";
+import Logos from "../AboutSection/Logos";
 import Team from "../AboutSection/Team";
+import Cta from "../HeroSection/Cta";
 
 const About = () => {
   return (
     <>
       <Hero />
       <Team />
+      <Logos />
+      <Cta />
     </>
   );
 };
