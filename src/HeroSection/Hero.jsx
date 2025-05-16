d3658522c19615590472b26157643ea495ead249
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 02:57:25 +0300

    Update Hero.jsx

--- src/HeroSection/Hero.jsx
+++ src/HeroSection/Hero.jsx
@@ -1,26 +1,26 @@
 const Hero = () => {
   return (
-    <header className="w-full bg-primary-midnight-green p-20 relative">
-      <div className="grid grid-cols-2 justify-between items-center container gap-[2rem]">
+    <header className="w-full bg-primary-midnight-green p-20 relative max-sm:p-10">
+      <div className="grid grid-cols-2 justify-between items-center container gap-[2rem] max-sm:grid-cols-1 max-sm:py-30">
         <img
           src="./assets/bg-pattern-home-1.svg"
           alt="img"
-          className="size-[10rem] -left-15 bottom-30 absolute "
+          className="size-[10rem] -left-15 bottom-30 absolute max-sm:hidden "
         />
-        <h1 className="text-[100px] font-bold text-primary-white leading-[6.5rem]">
-          Find the best <spn className="text-primary-light-coral">talent</spn>
+        <h1 className="text-[6rem] font-bold text-primary-white leading-[6.5rem] max-sm:text-center ">
+          Find the best <span className="text-primary-light-coral">talent</span>
         </h1>
 
         <div>
-          <div className=" h-1 w-[60px] bg-secondary-rapture-blue mb-10" />
-          <p className="font-semibold text-primary-white leading-small">
+          <div className=" h-1 w-[60px] bg-secondary-rapture-blue mb-10 max-sm:hidden " />
+          <p className="font-semibold text-primary-white leading-small max-sm:text-3xl max-sm:text-center">
             Finding the right people and building high performing teams can be
             hard. Most companies aren’t tapping into the abundance of global
             talent. We’re about to change that.
           </p>
           <img
             src="./assets/bg-pattern-home-2.svg"
-            className="absolute right-15 bottom-0"
+            className="absolute right-15 bottom-0 max-sm:w-[80%]"
           />
         </div>
       </div>
