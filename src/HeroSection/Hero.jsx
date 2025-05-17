c87804d84c95809b1a5bfa0c7ab208bbdd12d685
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-18 00:55:49 +0300

    Update Hero.jsx

--- src/HeroSection/Hero.jsx
+++ src/HeroSection/Hero.jsx
@@ -7,20 +7,20 @@ const Hero = () => {
           alt="img"
           className="size-[10rem] -left-15 bottom-30 absolute max-sm:hidden max-md:hidden "
         />
-        <h1 className="text-[6rem] font-bold text-primary-white leading-[6.5rem] max-sm:text-center max-md:text-center ">
+        <h1 className="text-[6rem] font-bold text-primary-white leading-[6.5rem] max-sm:text-center max-md:text-center max-sm:line-clamp max-sxm:font-semibold">
           Find the best <span className="text-primary-light-coral">talent</span>
         </h1>
 
         <div>
           <div className=" h-1 w-[60px] bg-secondary-rapture-blue mb-10 max-sm:hidden max-md:hidden " />
-          <p className="font-semibold text-primary-white leading-small max-sm:text-3xl max-sm:text-center max-md:text-3xl max-md:text-center">
+          <p className="font-semibold text-primary-white leading-small max-sm:text-xl max-sm:text-center max-md:text-3xl max-md:text-center max-sm:line-clamp-6">
             Finding the right people and building high performing teams can be
             hard. Most companies aren’t tapping into the abundance of global
             talent. We’re about to change that.
           </p>
           <img
             src="./assets/bg-pattern-home-2.svg"
-            className="absolute right-15 bottom-0 max-sm:w-[50%] max-md:left-50 max-md:right-50  max-md:w-[50%]"
+            className="absolute right-15 bottom-0 max-sm:mx-auto max-sm:w-[50%] max-md:left-50 max-md:right-50  max-md:w-[50%]"
           />
         </div>
       </div>
