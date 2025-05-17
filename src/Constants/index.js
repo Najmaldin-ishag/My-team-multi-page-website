11c02cb1b3656d2ffef2ba16880d00e9fb73c411
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 03:31:14 +0300

    Update Index.css

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -76,6 +76,33 @@ export const socailLinks = [
   },
 ];
 
+export const MobileFeatures = [
+  {
+    id: "1",
+    imgPath: "./assets/icon-person.svg",
+    alt: "icon",
+    title: "Easy to Implement",
+    description:
+      "Our processes have been refined over years of implementation meaning our teams always deliver",
+  },
+  {
+    id: "2",
+    imgPath: "./assets/icon-cog.svg",
+    alt: "icon",
+    title: "Enhanced Productivity Our",
+    description:
+      " Our customized platform with in-built analytics helps you manage your distributed teams",
+  },
+  {
+    id: "3",
+    imgPath: "./assets/icon-chart.svg",
+    alt: "icon",
+    title: "Experienced Individuals",
+    description:
+      "Our network is made up of highly experienced professionals who are passionate about what they do",
+  },
+];
+
 // About
 
 export const TeamMember = [
