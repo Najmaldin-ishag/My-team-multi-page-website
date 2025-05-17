5a95f0e809e0d8ddda68111654da903ee8f09345
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 03:37:22 +0300

    Update Footer.jsx

--- src/HeroSection/Footer.jsx
+++ src/HeroSection/Footer.jsx
@@ -4,13 +4,17 @@ const date = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="w-full bg-secondary-dark-green py-[2.5rem]">
-      <div className="grid grid-cols-3 gap-[.5rem] container">
+      <div className="grid grid-cols-3 gap-[.5rem] container max-sm:grid-cols-1 max-sm:items-center max-sm:text-center">
         <div>
-          <img src="./assets/logo.svg" alt="logo" className="mb-6" />
-          <div className="flex items-center gap-8">
+          <img
+            src="./assets/logo.svg"
+            alt="logo"
+            className="mb-6 max-sm:mx-auto"
+          />
+          <div className="flex items-center gap-8 max-sm:block max-sm:space-x-10 max-sm:mb-10 ">
             <Link
               to="/"
-              className="text-primary-white text-m font-semibold hover:text-primary-light-coral"
+              className="text-primary-white text-m font-semibold hover:text-primary-light-coral "
             >
               Home
             </Link>
@@ -34,7 +38,7 @@ const Footer = () => {
             <a href="tel:9498337432">949-833-7432</a>
           </address>
         </div>
-        <div className="flex flex-col items-center gap-4 ml-auto">
+        <div className="flex flex-col items-center gap-4 ml-auto max-sm:ml-0 max-sm:mt-10">
           <div className="flex items-center gap-4 mb-auto">
             {socailLinks.map((link) => (
               <img key={link.id} src={link.imgPath} alt="icon" />
