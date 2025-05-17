cf35f7c99fc9183f012bb569270a389590d06595
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 05:28:35 +0300

    Update Footer.jsx

--- src/HeroSection/Footer.jsx
+++ src/HeroSection/Footer.jsx
@@ -4,7 +4,7 @@ const date = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="w-full bg-secondary-dark-green py-[2.5rem]">
-      <div className="grid grid-cols-3 gap-[.5rem] container max-sm:grid-cols-1 max-sm:items-center max-sm:text-center">
+      <div className="grid grid-cols-3 gap-[.5rem] container max-sm:grid-cols-1 max-sm:items-center max-sm:text-center max-md:hidden max-sm:block">
         <div>
           <img
             src="./assets/logo.svg"
@@ -45,6 +45,49 @@ const Footer = () => {
             ))}
           </div>
 
+          <p className="text-primary-white font-thin text-sm">
+            {date} All Rights Reserved
+          </p>
+        </div>
+      </div>
+
+      {/* Tablet Footer */}
+      <div className="lg:hidden max-sm:hidden max-md:grid grid-cols-2 p-10">
+        <div>
+          <img
+            src="./assets/logo.svg"
+            alt="logo"
+            className="mb-6 max-sm:mx-auto"
+          />
+          <Link
+            to="/"
+            className="text-primary-white text-m font-semibold hover:text-primary-light-coral mr-10 "
+          >
+            Home
+          </Link>
+          <Link
+            className="text-primary-white text-m font-semibold hover:text-primary-light-coral"
+            to="/about"
+          >
+            About
+          </Link>
+          <div className="flex items-center gap-10 mt-10">
+            {socailLinks.map((link) => (
+              <img key={link.id} src={link.imgPath} alt="icon" />
+            ))}
+          </div>
+        </div>
+        <div className="ml-auto">
+          <address className="text-primary-white font-[200]">
+            987 Hillcrest
+            <br />
+            Irvine, CA
+            <br />
+            California 92714
+            <br />
+            <strong className="font-normal">Call Us:</strong>
+            <a href="tel:9498337432">949-833-7432</a>
+          </address>
           <p className="text-primary-white font-thin text-sm">
             {date} All Rights Reserved
           </p>
