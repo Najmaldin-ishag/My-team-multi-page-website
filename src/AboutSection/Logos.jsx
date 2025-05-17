68d1cf29d4403bbb8375e57dde1fbc3714eba6f6
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 23:09:15 +0300

    Update Logos.jsx

--- src/AboutSection/Logos.jsx
+++ src/AboutSection/Logos.jsx
@@ -2,12 +2,12 @@ import { LogosIcons } from "../Constants";
 
 const Logos = () => {
   return (
-    <section className="w-full bg-secondary-dark-green py-[6.5rem] px-0">
+    <section className="w-full bg-secondary-dark-green py-[6.5rem] px-0 ">
       <div className="container">
         <h2 className="text-primary-white text-[3rem] text-center font-semibold mb-8 max-sm:text-4xl max-sm:mb-10">
           Some Of our clients
         </h2>
-        <div className="flex items-center justify-around max-sm:flex-col max-sm:gap-10">
+        <div className="flex items-center justify-around max-sm:flex-col max-sm:gap-10 max-md:gap-10">
           {LogosIcons.map((icon) => (
             <img
               src={icon.imgPath}
