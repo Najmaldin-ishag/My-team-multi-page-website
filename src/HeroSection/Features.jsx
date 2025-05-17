2d726e1f0abd805712f03919557006c2d21de0f2
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-17 05:28:27 +0300

    Update Features.jsx

--- src/HeroSection/Features.jsx
+++ src/HeroSection/Features.jsx
@@ -2,11 +2,16 @@ import { features, icons, MobileFeatures } from "../Constants";
 
 const Features = () => {
   return (
-    <section className="w-full bg-secondary-dark-green p-[5rem]">
-      <div className="container grid grid-cols-2 justify-between items-center max-sm:hidden">
+    <section className="w-full bg-secondary-dark-green p-[10rem] relative">
+      <div className="container grid grid-cols-2 justify-between items-center max-sm:hidden max-md:grid-cols-1">
         <div>
-          <div className=" h-1 w-[60px] bg-primary-light-coral mb-10" />
-          <p className="font-bold text-primary-white text-[2rem] leading-[2.2rem]">
+          <div className=" h-1 w-[60px] bg-primary-light-coral mb-10 " />
+          <img
+            src="./assets/bg-pattern-about-4.svg"
+            alt="bg img"
+            className="max-md:top-0 max-md:right-0 absolute bottom-0 -right-30"
+          />
+          <p className="font-bold text-primary-white text-[2rem] leading-[2.2rem] max-md:mb-20">
             Build & manage
             <br />
             distributed teams <br /> like no one else.
