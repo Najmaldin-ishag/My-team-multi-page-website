63b8a3b954bb59375335aa006243593d91abf8a0
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-18 00:55:40 +0300

    Update features.jsx

--- src/HeroSection/Features.jsx
+++ src/HeroSection/Features.jsx
@@ -2,7 +2,7 @@ import { features, icons, MobileFeatures } from "../Constants";
 
 const Features = () => {
   return (
-    <section className="w-full bg-secondary-dark-green p-[10rem] relative">
+    <section className="w-full bg-secondary-dark-green p-[10rem] max-sm:p-10 relative">
       <div className="container grid grid-cols-2 justify-between items-center max-sm:hidden max-md:grid-cols-1">
         <div>
           <div className=" h-1 w-[60px] bg-primary-light-coral mb-10 " />
@@ -43,24 +43,26 @@ const Features = () => {
       </div>
 
       {/* Mobile Features */}
-      <div className=" lg:hidden md:hidden">
-        <div className=" h-1 w-[60px] bg-primary-light-coral mb-10" />
-        <p className="font-bold text-primary-white text-[2rem] leading-[2.2rem]">
-          Build & manage
-          <br />
-          distributed teams <br /> like no one else.
+      <div className=" lg:hidden md:hidden ">
+        <div className="  bg-primary-light-coral mb-10 " />
+        <p className="font-semibold text-primary-white text-[2rem] ">
+          Build & manage distributed teams like no one else.
         </p>
         <div>
           {MobileFeatures.map((item) => (
             <div
               key={item.id}
-              className="grid grid-cols-1 justify-between items-center mt-10"
+              className="flex flex-col  item-center mt-10  items-start max-w-[20rem]  "
             >
-              <img src={item.imgPath} alt={item.id} className="mx-auto mb-5 " />
-              <h3 className="text-primary-light-coral font-semibold text-md mb-2 text-center">
+              <img
+                src={item.imgPath}
+                alt={item.id}
+                className=" w-20 h-20 mx-auto mb-5 "
+              />
+              <h3 className="text-primary-light-coral font-semibold text-sm mb-2 mx-auto">
                 {item.title}
               </h3>
-              <p className="text-primary-white font-semibold">
+              <p className="text-primary-white font-semibold text-[1.5rem] text-center break-words whitespace-normal">
                 {item.description}
               </p>
             </div>
