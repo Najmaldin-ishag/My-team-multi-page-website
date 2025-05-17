3cfb0448dabead4435cdeb8d466e3014afe1943d
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-18 00:55:25 +0300

    Update Hero.jsx

--- src/AboutSection/Hero.jsx
+++ src/AboutSection/Hero.jsx
@@ -1,13 +1,13 @@
 const Hero = () => {
   return (
-    <header className="w-full bg-primary-midnight-green p-20 relative">
+    <header className="w-full bg-primary-midnight-green p-20 max-sm:p-10 relative">
       <div className="grid grid-cols-2 justify-between items-center container gap-[2rem] max-sm:grid-cols-1 max-sm:py-20 max-md:grid-cols-1 max-md:text-center">
-        <h1 className="text-[100px] font-bold text-primary-white leading-[6.5rem] max-sm:text-center">
+        <h1 className="text-[100px] font-bold text-primary-white leading-[6.5rem] max-sm:text-center max-sm:text-6xl">
           About
         </h1>
         <div>
-          <div className=" h-1 w-[60px] bg-primary-light-coral mb-10 max-sm:hidden max-md:hidden " />
-          <p className="font-semibold text-primary-white leading-small max-sm:text-center max-md:text-2xl">
+          <div className="  w-[60px] bg-primary-light-coral mb-10 max-sm:hidden max-md:hidden max-sm:px-[5rem]" />
+          <p className="font-semibold text-primary-white leading-small max-sm:text-center max-md:text-2xl max-sm:text-[1.5rem]">
             We help companies build dynamic teams made up of top global talent.
             Using our network of passionate professionals we drive innovation
             and deliver incredible outcomes. Talented, diverse teams shape the
